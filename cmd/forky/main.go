cc68f4c6438f0ac44c293b0c23bbc2a7dc343d1e
Thet Naing Tun <thetnaingtun.ucsy@gmail.com>
2023-06-04 15:12:43 +0700

    refactor: move main module to cmd

---
+++ cmd/forky/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"path/filepath"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/google/go-github/v52/github"
+	"github.com/thetnaingtn/forky/ui"
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := cli.NewApp()
+
+	app.Authors = []*cli.Author{{
+		Name: "thetnaingtn",
+	}}
+	app.Usage = "Update your forks with ease."
+
+	app.Flags = []cli.Flag{
+		&cli.StringFlag{
+			EnvVars: []string{"GITHUB_TOKEN"},
+			Name:    "token",
+			Usage:   "Your Github Token",
+			Aliases: []string{"t"},
+		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+
+		log.SetFlags(0)
+		f, err := tea.LogToFile(filepath.Join(os.TempDir(), "forky.log"), "")
+		if err != nil {
+			return cli.Exit(err.Error(), 1)
+		}
+		defer func() { _ = f.Close() }()
+
+		token := c.String("token")
+		if token == "" {
+			return cli.Exit("missing github token", 1)
+		}
+
+		ctx := context.Background()
+
+		client := github.NewTokenClient(ctx, token)
+
+		m := ui.NewAppModel(client)
+
+		p := tea.NewProgram(m, tea.WithAltScreen())
+
+		if _, err := p.Run(); err != nil {
+			return cli.Exit(err.Error(), 1)
+		}
+
+		return nil
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatalln(err)
+	}
+
+}
---
+++ forky.go
@@ -0,0 +1,121 @@
+package forky
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"sort"
+
+	"github.com/google/go-github/v52/github"
+)
+
+var (
+	pageSize = 100
+)
+
+type RepositoryWithDetails struct {
+	FullName      string
+	Description   string
+	RepoURL       string
+	Parent        string
+	ParentDeleted bool
+	Private       bool
+	BehindBy      int
+}
+
+func GetForks(ctx context.Context, client *github.Client) ([]*RepositoryWithDetails, error) {
+	var forks []*RepositoryWithDetails
+	repos, err := getAllRepositories(context.Background(), client)
+
+	if err != nil {
+		return forks, err
+	}
+
+	for _, r := range repos {
+		if !r.GetFork() {
+			continue
+		}
+		repo, resp, err := client.Repositories.Get(ctx, r.GetOwner().GetLogin(), r.GetName())
+
+		switch resp.StatusCode {
+		case http.StatusForbidden:
+			continue
+		case http.StatusUnavailableForLegalReasons:
+			forks = append(forks, buildDetails(r, nil, resp.StatusCode))
+			continue
+		}
+
+		if err != nil {
+			return forks, fmt.Errorf("failed to get repository %s: %w", r.GetName(), err)
+		}
+
+		parent := repo.GetParent()
+
+		base := fmt.Sprintf("%s:%s", parent.GetOwner().GetLogin(), parent.GetDefaultBranch())
+		head := fmt.Sprintf("%s:%s", repo.GetOwner().GetLogin(), repo.GetDefaultBranch())
+		cmpr, resp, err := client.Repositories.CompareCommits(
+			ctx,
+			repo.GetOwner().GetLogin(),
+			repo.GetName(),
+			base,
+			head,
+			&github.ListOptions{},
+		)
+
+		if err != nil && resp.StatusCode != http.StatusNotFound {
+			log.Println("ERR", err)
+			return forks, fmt.Errorf("failed to compare repository with parent %s: %w", parent.GetName(), err)
+		}
+
+		if cmpr == nil || cmpr.GetBehindBy() < 1 {
+			continue
+		}
+
+		forks = append(forks, buildDetails(repo, cmpr, resp.StatusCode))
+
+	}
+
+	sort.SliceStable(forks, func(i, j int) bool {
+		return forks[i].BehindBy > forks[j].BehindBy
+	})
+
+	return forks, nil
+}
+
+func buildDetails(repo *github.Repository, commit *github.CommitsComparison, code int) *RepositoryWithDetails {
+	return &RepositoryWithDetails{
+		FullName:      repo.GetFullName(),
+		Description:   repo.GetDescription(),
+		RepoURL:       repo.GetURL(),
+		Parent:        repo.GetParent().GetFullName(),
+		ParentDeleted: code == http.StatusNotFound,
+		Private:       repo.GetPrivate(),
+		BehindBy:      commit.GetBehindBy(),
+	}
+}
+
+func getAllRepositories(ctx context.Context, client *github.Client) ([]*github.Repository, error) {
+	var allRepos []*github.Repository
+	opts := &github.RepositoryListOptions{
+		Type:        "all",
+		ListOptions: github.ListOptions{PerPage: pageSize},
+	}
+
+	for {
+		repos, resp, err := client.Repositories.List(ctx, "thetnaingtn", opts)
+		if err != nil {
+			return allRepos, err
+		}
+
+		allRepos = append(allRepos, repos...)
+
+		if resp.NextPage == 0 {
+			break
+		}
+
+		opts.Page = resp.NextPage
+	}
+
+	return allRepos, nil
+}

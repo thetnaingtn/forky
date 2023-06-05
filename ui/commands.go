dd26fc56ce6908cd917b4225b4dabd6c8daf3f65
Thet Naing Tun <thetnaingtun.ucsy@gmail.com>
2023-06-05 17:39:12 +0700

    feat: add request merge repo and merge repo commands

--- ui/commands.go
+++ ui/commands.go
@@ -2,6 +2,8 @@ package ui
 
 import (
 	"context"
+	"log"
+	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/google/go-github/v52/github"
@@ -12,9 +14,28 @@ func enqueuegetReposListCmd() tea.Msg {
 	return getReposListMsg{}
 }
 
+func requestMergeReposCmd() tea.Msg {
+	return mergeSelectedRepos{}
+}
+
 func getReposCmd(client *github.Client) tea.Cmd {
 	return func() tea.Msg {
 		repos, _ := forky.GetForks(context.Background(), client)
 		return gotReposListMsg{repos: repos}
+	}
+}
+
+func mergeReposCmd(client *github.Client, repos []*forky.RepositoryWithDetails) tea.Cmd {
+	return func() tea.Msg {
+		var names []string
+		for _, repo := range repos {
+			names = append(names, repo.Name)
+		}
+		log.Printf("mergeReposCmd: %s", strings.Join(names, ", "))
+
+		if err := forky.SyncBranchWithUpstreamRepo(client, repos); err != nil {
+		}
+
+		return mergedSelectedRepos{}
 	}
 }
